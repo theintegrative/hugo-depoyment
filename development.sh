e3c36e4f2aa1aca27ceb38661ee443123b38c1b7
brightly <theintegrative@gmail.com>
2022-08-20 21:31:33 +0200

    renamed vault-name

--- development.sh
+++ development.sh
@@ -5,7 +5,7 @@ brave-browser http://localhost:1313/
 
 # With flatpak start obsidian and open the vault "en" 
 # that is the content folder through the obsidian URI in the background
-flatpak run md.obsidian.Obsidian obsidian://open?vault=en &
+flatpak run md.obsidian.Obsidian obsidian://open?vault=vault-name &
 
 # Starting development server with hugo doks theme for more https://getdoks.org/ 
 # For hugo use: hugo server
